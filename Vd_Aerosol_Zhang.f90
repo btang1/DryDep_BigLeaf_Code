2c09de55f057bafaac4199a9230c48771e8d8f16
Beiming Tang <51177339+btang1@users.noreply.github.com>
2023-05-09 08:51:25 -0400

    vd_zhang_aerosol

---
+++ Vd_Aerosol_Zhang.f90
@@ -0,0 +1,367 @@
+subroutine Vd_Aerosol_Zhang(z2,zl,z0_f,ustar,diam,rhop,ts,t2,mlu,lai_f,vd)
+
+!Developed by Dr.Beiming Tang based on CAMx code
+!NOAA ARL UFS project
+!05/09/2023
+
+!Modifications:
+
+!Input arguments:
+!    z2      reference height                     (unit = m)
+!    zl      Z/L stability parameter              (unit = 1)
+!    z0_f    surface roughness                    (unit = m)
+!    ustar   friction velocity                    (unit = m/s)
+!    diam    log-mean sectional aerosol diameter  (unit = m)
+!    rhop    aerosol density                      (unit = g/m^3)
+!    ts      surface temperature                  (unit = K)
+!    t2      temperature at z2                    (unit = K)
+!    mlu     land use index                       (unit = 1)
+!    lai_f   leaf area index                      (unit = 1)
+
+!Output arguments:
+!    vd      deposition velocity                  (unit = cm/s)
+
+!Routine called:
+!    dryvd_pm
+
+!Called by:
+!    future dry deposition code in UFS
+
+    implicit none
+
+!Step 0. Define variables type & constant values
+!!Step 0-1. Define global varibles type
+    real    :: z2
+    real    :: zl
+    real    :: z0_f
+    real    :: ustar
+    real    :: diam
+    real    :: rhop
+    real    :: ts
+    real    :: t2
+    integer :: mlu
+    real    :: lai_f
+    real    :: vd
+
+!!Step 0-2. Define local variable type
+    real    :: vdp
+
+!
+!Main Program Start
+!
+
+!Step 1. Call drt deposition velocity algorithm
+    call dryvd_pm(z2,zl,z0_f,utar,t2,ts,lai_f,mlu,vdp,diam,rhop)
+    vd = vdp
+    
+    return
+    end
+
+
+
+
+
+
+subroutine dryvd_pm(z2,zl,z0_f,utar,t2,ts,lai_f,mlu,vdp,diam,rhoprt)
+
+!LUC No.        Vegetation type
+!    1          water
+!    2          ice
+!    3          inland lake
+!    4          evengreen needleleaf trees
+!    5          evengreen broadleaf  trees
+!    6          deciduous needlelead trees
+!    7          deciduous broadleaf  trees
+!    8          tropical broadleaf   trees
+!    9          drought deciduous    trees
+!   10          evergreen broadlead  shrubs
+!   11          deciduous            shrubs
+!   12          thorn                shrubs
+!   13          short grass and forbs
+!   14          long grass
+!   15          crops
+!   16          rice
+!   17          sugar
+!   18          maize
+!   19          cotton
+!   20          irrigated crops
+!   21          urban
+!   22          tundra
+!   23          swamp
+!   24          desert
+!   25          mixed wood forests
+!   26          transitional forest
+
+!Input argiments
+!    z2         meteorology reference height          (unit = m)
+!    zl         Z/L stability parameter               (unit = 1)
+!    z0_f       surface roughness                     (unit = m)
+!    ustar      friction velocity                     (unit = m/s)
+!    t2         temperature at Z2                     (unit = K)
+!    ts         surface temperature                   (unit = K)
+!    lai_f      leaf area index                       (unit = 1)
+!    mlu        land use type                         (unit = 1)
+!    vdp        particle dry deposition velocity      (unit = m/s)
+!    diam       log-mean sectional aerosol diameter   (unit = m)
+!    rhoprt     aerosol density                       (unit = g/m^3) 
+
+!    ra         aerosol resistance                    (unit = s/m)
+!    aest       parameter for calculating EIM         (unit = )
+!    binsize    radius of a size bin                  (unit = )
+!    eb         Brownian collection efficiency        (unit = )
+!    eim        impaction collection efficiency       (unit = )
+!    ein        interception collection efficiency    (unit = )
+!    gama       parameter for calculating EB          (unit = )
+!    pllp       leaf dimension for calculating EIN    (unit = )
+
+!Output argument:
+!    vdp        deposition velocity                   (unit = m/s)
+
+
+    implicit none
+
+    include 'camx.prm'        !I think we need to replace this with our ufs inputs
+    inclde  'deposit.inc'     !I think we need to replace this with our ufs inputs
+
+!Step 0. Define input variables type & value constant
+!!Step 0-1. Define global variables
+    real    :: z2
+    real    :: zl
+    real    :: z0_f
+    real    :: ustar
+    real    :: t2
+    real    :: ts
+    real    :: lai_f
+    integer :: mlu
+    real    :: vdp
+    real    :: diam
+    real    :: rhopt
+
+!!Step 0-2. Define local varible
+    integer :: i
+    real    :: rhop
+    real    :: ra
+    real    :: pllp
+    real    :: binsize
+    real    :: tave
+    real    :: amu
+    real    :: anu
+    real    :: prii
+    real    :: priiv
+    real    :: vphil
+    real    :: cfac
+    real    :: taurel
+    real    :: amob
+    real    :: diff
+    real    :: schm
+    real    :: pdepv
+    real    :: st
+    real    :: eb
+    real    :: eim
+    real    :: ein
+    real    :: r1
+    real    :: rs
+    real    :: vdsize
+    
+!!Step 0-3. Define constant values
+    real,parameter    :: aa1    = 1.257
+    real,parameter    :: aa2    = 0.4
+    real,parameter    :: aa3    = 1.1
+    real,parameter    :: amfp   = 6.53e-8
+    real,parameter    :: roarow = 1.19
+    real,parameter    :: boltzk = 1.3806044503487214e-23
+    real,parameter    :: dair = 0.369*29.+6.29
+    real,parameter    :: dh2o = 0.369*18.+6.29
+
+    real,dimension(26)    :: aest  
+    real,dimension(26)    :: pllp1 !mim leaf dimension for each LUC
+    real,dimension(26)    :: pllp2 !max leaf dimension for each LUC
+    real,dimension(26)    :: gama
+
+    aset  = (/100., 50.,100., 1.0, 0.8, 1.1, 0.8, 0.6, 1.0, 1.1,  &
+               1.1, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.1, 1.2, 1.2,  &
+               1.5, 50., 2.0, 50., 0.8, 0.8/)
+    pllp1 = (/-0.9,-0.9,-0.9, 2.0, 5.0, 2.0, 5.0, 5.0, 5.0, 5.0,  &
+               5.0, 2.0, 2.0, 2.0, 2.0, 2.0, 5.0, 5.0, 5.0, 2.0,  &
+              10.0,-0.9,10.0,-0.9, 5.0, 5.0/)
+    pllp2 = (/-0.9,-0.9,-0.9, 2.0, 5.0, 5.0,10.0, 5.0,10.0, 5.0,  &
+              10.0, 5.0, 5.0, 5.0, 5.0, 5.0,10.0,10.0,10.0, 5.0,  &
+              10.0,-0.9,10.0,-0.9, 5.0, 5.0/)
+    gama  = (/ 0.5,0.54, 0.5,0.56,0.56,0.56,0.56,0.58,0.56,0.55,  &
+              0.55,0.54,0.54,0.55,0.54,0.54,0.54,0.55,0.54,0.54,  &
+              0.56,0.54,0.54,0.54,0.56,0.56/)
+    
+    !particle density          
+    RHOP  = rhoprt*1.0e-3           !Covert a unit from g/m^3 to kg/m^3
+
+    vdp   = 0.0    
+    i     = mlu     
+
+!
+!Main Program Start
+!
+
+
+!Step 1. Calculate aerodynamic resistance 
+    if (zl >= 0) then
+        ra = (0.74*log(z2/z0_f) + 4.7 *zl)/(0.4*ustar)
+    else
+        ra = 0.74*(log(z2/z0_f) - 2*log((1+sqrt(1-9.*zl))*0.5))/(0.4*ustar)
+    endif
+    
+    ra = amax1(ra,5.0)
+    
+    if (i == 1 .or. i == 3) then   !for water or inland lake
+        ra = amin1(ra,2000.)
+    else
+        ra = amin1(ra,1000.)
+    endif
+
+!Step 2. Loop for partical species ???
+    if (lai_ref(i,15) == lat_ref(i,14)) then    !lai_ref IS NOT DEFINED
+        pllp = pllp2(i)
+    else
+        pllp = pllp2(i) - (lai_f-lai_ref(i,14))/(lai_ref(i,15)-lai_ref(i,14)+1.e-10)*(pllp2(i)-pllp1(i))
+    endif
+
+    !calculate radius of particle
+    binsize  = diam/2
+
+    tave     = 0.5*(t2 + ts)
+
+    !calculate air dynamic viscosity
+    amu      = 145.8 * 1.e-8 * tave ** 1.5/ (tave + 110.4)
+
+    !calculate air kinetic viscosity
+    anu      = amu/roarow
+  
+    !calculate cunningham slip correction factor & relaxation time 
+    
+    prii     = 2.* 9.81 /(9. * amu)
+    priiv    = prii * (RHOP -roarow)
+    vphil    = 0.
+    cfac     = 1. + amfp/binsize*(aa1+aa2*exp(-aa3*binsize/amfp))
+    taurel   = amax1(priiv*binsize**2*cfac/9.81,0)
+
+    !calculate stokes friction and diffusion coefficients
+    amob     = 6. * 3.14 * amu *binsize /cfac
+    diff     = boltzk *tave /amob
+    schm     = anu/diff
+
+    !calculate gravitational settling velocity
+    pdepv    = taurel * 9.81
+
+    !calculate efficiency by diffusion, impaction, interception and particle rebound
+    !!smooth surface
+    if (pllp <= 0) then
+        st   = taurel*ustar*ustar/anu
+    !!vegetated surface
+    else
+        st   = taurel*ustar/pllp*1000
+    endif
+
+    eb       = schm **(-gama(i))
+    eim      = (st/(st+aest(i)))**2
+    ein      = 0.0
+    if (pllp >= 0.00) then
+        ein  = (1000.*2.*binsize/pllp)**2*0.5
+    end if
+
+    r1       = exp(-st**0.5)
+    
+    if (r1 <= 0.5) then
+        r1   = 0.5
+    rs       = 1./(3.*ustar*r1*(eb + eim + ein))
+
+!Step 3. Calculate deposition velocity
+    vdsize   = prepv + 1./(ra + rs)
+    vdp      = vdsize
+
+    return
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
