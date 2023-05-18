8da35cd48ccd4669eb148b5c74eee39f72dae67a
Beiming Tang <51177339+btang1@users.noreply.github.com>
2023-05-18 05:37:18 -0400

    Update module_vd_constants.f90

--- module_vd_constants.f90
+++ module_vd_constants.f90
@@ -2,15 +2,15 @@ module vd_constants
     implicit none
 
     ! DEFINE CONSTANTS USED IN CALCULATIONS 
-    real, parameter  :: vk = 0.4         ! Add comments and units to these
-    real, parameter :: rmin = 1.0        ! Add comments and units to these
-    real, parameter :: rmax = 1.e5       ! Add comments and units to these
-    real, parameter :: xmfp = 6.5e-8     ! Add comments and units to these
-    real, parameter :: g = 9.8           ! Add comments and units to these
-    real, parameter :: vabs = 1.81e-2    ! dynamic viscosity of air
-    real, parameter :: boltz = 1.38e-20  ! Add comments and units to these
-    real, parameter :: pi = 3.1415927    ! Add comments and units to these
-    real, parameter :: vair = 1.5e-5     ! kinetic viscosity of air
-    real, parameter :: diffh2o = 2.3e-5  ! Add comments and units to these
+    real, parameter  :: vk = 0.4         ! Von Karman constant                 (1)
+    real, parameter :: rmin = 1.0        ! minimum resistance                  (?)
+    real, parameter :: rmax = 1.e5       ! maximum resistance                  (?)
+    real, parameter :: xmfp = 6.5e-8     ! mean free path                      (?)
+    real, parameter :: g = 9.8           ! gravitational acceleration constant (1)
+    real, parameter :: vabs = 1.81e-2    ! dynamic viscosity of air            (g/m-s)
+    real, parameter :: boltz = 1.38e-20  ! Boltzmann constant.                 (m^2*g/(s^2*K))
+    real, parameter :: pi = 3.1415927    ! Circle constant.                    (1)
+    real, parameter :: vair = 1.5e-5     ! kinematic viscosity of air          (m^2/s)   
+    real, parameter :: diffh2o = 2.3e-5  ! 
 
 end module vd_constants
