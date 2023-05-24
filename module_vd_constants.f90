9a4cddefabf9feb522a923a2a3e0beab1a3dce28
Beiming Tang <51177339+btang1@users.noreply.github.com>
2023-05-24 05:42:40 -0400

    Update module_vd_constants.f90

--- module_vd_constants.f90
+++ module_vd_constants.f90
@@ -20,7 +20,13 @@ module vd_constants
     real, parameter :: dair = 0.369*29.+6.29
     real, parameter :: dh2o = 0.369*18.+6.29
 
-    !Baseline resistances for Wesely (1989) == 11 land time * 5 seasons
+    real,parameter   :: rhoh2o = 1.e6                  !water density     (g/m^3)
+    real,parameter   :: rmax_zhang = 100.0                   !max resistance    (s/m)
+    real*8,parameter :: CP_A = 2.7457D15               !Parameters for Bi-Di NH3 drydep, Whaley et al., (2018),constant A for compensation point Eq (K-ug/m^3)
+    real*8,parameter :: CP_B = 10378.D0                !Parameters for Bi-Di NH3 drydep, Whaley et al., (2018),constant B for compensation point Eq (K)
+  
+  
+  !Baseline resistances for Wesely (1989) == 11 land time * 5 seasons
     data rj  /9999.,  60., 120.,  70., 130., 100.,9999.,9999., 80., 100., 150.,   &
               9999.,9999.,9999.,9999., 250., 500.,9999.,9999.,9999.,9999.,9999.,  &
               9999.,9999.,9999.,9999., 250., 500.,9999.,9999.,9999.,9999.,9999.,  &
@@ -63,7 +69,7 @@ module vd_constants
               9999.,1000.,1000., 400.,1500., 600.,9999.,9999., 800.,1000., 800.,  &
               9999.,1000., 500., 500.,1500., 700.,9999.,9999., 600., 800., 800./ 
     
-    !Define array of constants used in Zhang model
+    !Define array of constants used in Zhang aerosol model
     real,dimension(26)    :: aest  
     real,dimension(26)    :: pllp1                         !mim leaf dimension for each LUC
     real,dimension(26)    :: pllp2                         !max leaf dimension for each LUC
@@ -82,4 +88,147 @@ module vd_constants
     gama  = (/ 0.5,0.54, 0.5,0.56,0.56,0.56,0.56,0.58,0.56,0.55,  &
                       0.55,0.54,0.54,0.55,0.54,0.54,0.54,0.55,0.54,0.54,  &
                       0.56,0.54,0.54,0.54,0.56,0.56/)
+                      
+                      
+    !Define arrady of constants in Zhang gas model
+    real*8, dimension(26)  :: gamst                      ! Stomatal emission potential for NH3 [(mol/L)/(mol/L)]
+    real*8, dimension(26)  :: gamg                       ! Ground emission potential for NH3 [(mol/L)/(mol/L)]
+    real, dimension(26)    :: Rac1                      ! In-canopy aerodynamic resistance minimum for Rac0 (s/m)
+    real, dimension(26)    :: Rac2                      ! In-canopy aerodynamic resistance maximum for Rac0 (s/m)
+    real, dimension(26)    :: RcutdO                    ! dry cuticle resistance for o3 (s/m)
+    real, dimension(26)    :: RcutwO                    ! wet cuticle resistance for o3 (s/m)
+    real, dimension(26)    :: RgO                       ! ground resistance for o3 (s/m)
+    real, dimension(26)    :: RcutdS                    ! Dry cuticle resistance of so2 (s/m)    
+    real, dimension(26)    :: RgS                       ! Ground resistance for SO2 (s/m)
+    real, dimension(26)    :: rsmin                     !Stomatal resistance related paramters
+    real, dimension(26)    :: brs                     !Stomatal resistance related paramters
+    real, dimension(26)    :: tmin                     !Stomatal resistance related paramters
+    real, dimension(26)    :: tmax                     !Stomatal resistance related paramters
+    real, dimension(26)    :: topt                     !Stomatal resistance related paramters
+    real, dimension(26)    :: bvpd                     !Stomatal resistance related paramters
+    real, dimension(26)    :: psi1                     !Stomatal resistance related paramters
+    real, dimension(26)    :: psi2                     !Stomatal resistance related paramters
+    real, dimension(26)    :: sdmax                    !Maximum snow depth over which snow fraction for leaves is 1.0,Snow fraction for ground is treated 2 times of that for leaves
+    
+    gamst = (/ 0.D0,    0.D0,    0.D0, 3000.D0, 3000.D0, 300.D0,  300.D0,  300.D0,  300.D0,  300.D0,    & !   LU# 01 - 10
+             300.D0,  300.D0,  300.D0,  300.D0,  800.D0, 800.D0,  800.D0,  800.D0,  800.D0,  800.D0,    & !   LU# 11 - 20
+               0.D0,   20.D0,  100.D0,    0.D0, 1000.D0, 300.D0 /)                                        !   LU# 21 - 26           
+  
+    gamg  = (/ 0.D0,    0.D0,    0.D0, 1000.D0, 1000.D0, 1000.D0,  200.D0,   20.D0,  500.D0,   20.D0,   & !   LU# 01 - 10
+             200.D0,   20.D0, 2000.D0, 2000.D0,  800.D0,  800.D0,  800.D0,  800.D0,  800.D0,  800.D0,   & !   LU# 11 - 20
+               0.D0,   20.D0,   20.D0,    0.D0, 1000.D0,   20.D0 /)                                       !   LU# 21 - 26
+               
+    Rac1  = (/0   ,  0   ,  0   ,  100 ,  250 ,  60  ,  100 ,  300 ,  100 ,  60  ,    &
+              20  ,  40  ,  20  ,  10  ,  10  ,  10  ,  10  ,  10  ,  10  ,  20  ,    &
+              40  ,  0   ,  20  ,  0   ,  100 ,  100    /)
+             
+    Rac2  = (/0   ,  0   ,  0   ,  100 ,  250 ,  100 ,  250 ,  300 ,  100 ,  60  ,
+              60  ,  40  ,  20  ,  40  ,  40  ,  40  ,  40  ,  50  ,  40  ,  20  ,
+              40  ,  0   ,  20  ,  0   ,  100 ,  100    /)
+    
+         DATA   RcutdO          /
+     & -999 , -999 , -999 , 4000 , 6000 , 
+     & 4000 , 6000 , 6000 , 8000 , 6000 ,
+     & 5000 , 5000 , 4000 , 4000 , 4000 ,
+     & 4000 , 4000 , 5000 , 5000 , 4000 ,
+     & 6000 , 8000 , 5000 , -999 , 4000 ,
+     & 4000    /
+      DATA   RcutwO          /
+     & -999 , -999 , -999 ,  200 ,  400 , 
+     &  200 ,  400 ,  400 ,  400 ,  400 ,
+     &  300 ,  300 ,  200 ,  200 ,  200 ,
+     &  200 ,  200 ,  300 ,  300 ,  200 ,
+     &  400 ,  400 ,  300 , -999 ,  200 ,
+     &  200    /
+          DATA   RgO            /
+     & 2000 , 2000 , 2000 ,  200 ,  200 , 
+     &  200 ,  200 ,  200 ,  200 ,  200 ,
+     &  200 ,  200 ,  200 ,  200 ,  200 ,
+     &  200 ,  200 ,  200 ,  200 ,  500 ,
+     &  500 ,  500 ,  500 ,  500 ,  200 ,
+     &  200    /           
+      DATA   RcutdS          /
+     & -999 , -999 , -999 , 2000 , 2500 , 
+     & 2000 , 2500 , 2500 , 6000 , 2000 ,
+     & 2000 , 2000 , 1000 , 1000 , 1500 ,
+     & 1500 , 2000 , 2000 , 2000 , 2000 ,
+     & 4000 , 2000 , 1500 , -999 , 2500 ,
+     & 2500    /               
+          DATA   RgS            /
+     &   20 ,   70 ,  20  ,  200 ,  100 , 
+     &  200 ,  200 ,  100 ,  300 ,  200 ,
+     &  200 ,  200 ,  200 ,  200 ,  200 ,
+     &   50 ,  200 ,  200 ,  200 ,   50 ,
+     &  300 ,  300 ,   50 ,  700 ,  200 ,
+     &  200    /         
+            DATA   rsmin            /
+     & -999 , -999 , -999 ,  250 ,  150 , 
+     &  250 ,  150 ,  150 ,  250 ,  150 , 
+     &  150 ,  250 ,  150 ,  100 ,  120 , 
+     &  120 ,  120 ,  250 ,  125 ,  150 ,
+     &  200 ,  150 ,  150 , -999 ,  150 , 
+     &  150   /
+      DATA   brs              /
+     & -999 , -999 , -999 ,   44 ,   40 , 
+     &   44 ,   43 ,   40 ,   44 ,   40 , 
+     &   44 ,   44 ,   50 ,   20 ,   40 , 
+     &   40 ,   50 ,   65 ,   65 ,   40 , 
+     &   42 ,   25 ,   40 , -999 ,   44 , 
+     &   43   /
+      DATA   tmin             /
+     & -999 , -999 , -999 ,   -5 ,    0 , 
+     &   -5 ,    0 ,    0 ,    0 ,    0 , 
+     &   -5 ,    0 ,    5 ,    5 ,    5 , 
+     &    5 ,    5 ,    5 ,   10 ,    5 , 
+     &    0 ,   -5 ,    0 , -999 ,   -3 , 
+     &    0   /
+      DATA   tmax             /
+     & -999 , -999 , -999 ,   40 ,   45 , 
+     &   40 ,   45 ,   45 ,   45 ,   45 , 
+     &   40 ,   45 ,   40 ,   45 ,   45 , 
+     &   45 ,   45 ,   45 ,   45 ,   45 , 
+     &   45 ,   40 ,   45 , -999 ,   42 , 
+     &   45   /
+      DATA   topt             /
+     & -999 , -999 , -999 ,   15 ,   30 , 
+     &   15 ,   27 ,   30 ,   25 ,   30 , 
+     &   15 ,   25 ,   30 ,   25 ,   27 , 
+     &   27 ,   25 ,   25 ,   30 ,   25 , 
+     &   22 ,   20 ,   20 , -999 ,   21 , 
+     &   25   /
+      DATA   bvpd             /
+     & -999 , -999 , -999 ,  0.31,  0.27, 
+     &  0.31,  0.36,  0.27,  0.31,  0.27,  
+     &  0.27,  0.27,  0.0 ,  0.0 ,  0.0 ,  
+     &  0.0 ,  0.0 ,  0.0 ,  0.0 ,  0.0 ,  
+     &  0.31,  0.24,  0.27, -999 ,  0.34, 
+     &  0.31   /
+      DATA   psi1            /
+     & -999 , -999 , -999 , -2.0 , -1.0 , 
+     & -2.0 , -1.9 , -1.0 , -1.0 , -2.0 ,
+     & -2.0 , -2.0 , -1.5 , -1.5 , -1.5 ,
+     & -1.5 , -1.5 , -1.5 , -1.5 , -1.5 ,
+     & -1.5 ,    0 , -1.5 , -999 , -2.0 ,
+     & -2.0    /
+      DATA   psi2            /
+     & -999 , -999 , -999 , -2.5 , -5.0 , 
+     & -2.5 , -2.5 , -5.0 , -4.0 , -4.0 ,
+     & -4.0 , -3.5 , -2.5 , -2.5 , -2.5 ,
+     & -2.5 , -2.5 , -2.5 , -2.5 , -2.5 ,
+     & -3.0 , -1.5 , -2.5 , -999 , -2.5 ,
+     & -3.0    /         
+          DATA SDMAX  / 
+     &              9999. ,  10.  , 9999. ,  200. ,  200. ,
+     &               200. , 200.  ,  200. ,  200. ,   30. ,
+     &                20. ,  30.  ,   10. ,   20. ,   10. ,
+     &                10. ,  10.  ,   10. ,   10. ,   10. ,
+     &               200. ,  10.  ,   10. ,   10. ,  200. ,
+     &               200.        /
+             
+               
+               
+               
+               
+               
+               
 end module vd_constants
